e7a958a918d0a177ee99d1d957647b2c57836490
hannasabrynna <hannasabrynna135@gmail.com>
2025-01-07 00:16:06 -0300

    feat:Leaflet on Mobile

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import * as Leaflet from 'leaflet';
+import type { Map, Marker, Circle } from 'leaflet';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class Tab2Page {
-
+export default class Tab2Page implements OnInit {
   constructor() {}
 
+  ngOnInit() {
+    setTimeout(() => {
+      const map: Map = Leaflet.map('map').fitWorld();
+
+      Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap',
+      }).addTo(map);
+
+      map.locate({ setView: true, maxZoom: 16 });
+
+      map.on('locationfound', (e: Leaflet.LocationEvent) => {
+        const radius = e.accuracy;
+
+        const marker: Marker = Leaflet.marker(e.latlng).addTo(map);
+        const circle: Circle = Leaflet.circle(e.latlng, {
+          radius,
+        }).addTo(map);
+
+      });
+
+      map.on('locationerror', (e: Leaflet.ErrorEvent) => {
+        alert(`Erro ao obter localização: ${e.message}`);
+      });
+
+    }, 300);
+  }
 }
+
--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -13,7 +13,7 @@ export const routes: Routes = [
       {
         path: 'tab2',
         loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+          import('../tab2/tab2.page'),
       },
       {
         path: 'tab3',
