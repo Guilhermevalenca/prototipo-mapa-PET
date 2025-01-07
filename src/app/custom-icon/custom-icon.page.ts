9057a029fc37f6449ebe4882569074743c2659c3
Guilherme Valença <guilherm3vrp@gmail.com>
2025-01-07 15:22:41 -0300

    feat: add icon extend and many icons

--- src/app/custom-icon/custom-icon.page.ts
+++ src/app/custom-icon/custom-icon.page.ts
@@ -15,30 +15,65 @@ export default class CustomIconPage implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    setTimeout(this.markersWithCustomIcons, 300);
+    setTimeout(() => this.markersWithCustomIcons(), 300);
   }
 
   private markersWithCustomIcons() {
     const map: Map = this.renderMapLeafletService.basic('map');
 
-    this.icon(map);
+    // this.icon(map);
+    this.manyIcons(map);
   }
 
   private icon(map: Map) {
     const greenIcon: Icon = Leaflet.icon({
-      iconUrl: 'leaf-green.png',
-      shadowUrl: 'leaf-shadow.png',
+      iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png',
+      shadowUrl: 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png',
 
-      iconSize:     [38, 95], // size of the icon
-      shadowSize:   [50, 64], // size of the shadow
-      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62],  // the same for the shadow
-      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      iconSize:     [38, 95],
+      shadowSize:   [50, 64],
+      iconAnchor:   [22, 94],
+      shadowAnchor: [4, 62],
+      popupAnchor:  [-3, -76]
     });
 
     Leaflet.marker([51.5, -0.09], {
       icon: greenIcon
     })
       .addTo(map);
+  }
+
+  private iconExtend() {
+    return Leaflet.Icon.extend({
+      options: {
+        shadowUrl: 'https://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+        iconSize:     [38, 95],
+        shadowSize:   [50, 64],
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76],
+      },
+    });
+  }
+
+  private manyIcons(map: Map) {
+    const LeafIcon = this.iconExtend();
+
+    //@ts-ignore
+    const greenIcon = new LeafIcon({iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png'});
+    //@ts-ignore
+    const redIcon = new LeafIcon({iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-red.png'});
+    //@ts-ignore
+    const orangeIcon = new LeafIcon({iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-orange.png'});
+
+    Leaflet.marker([51.5, -0.09], {icon: greenIcon})
+      .addTo(map)
+      .bindPopup("I am a green leaf.");
+    Leaflet.marker([51.495, -0.083], {icon: redIcon})
+      .addTo(map)
+      .bindPopup("I am a red leaf.");
+    Leaflet.marker([51.49, -0.1], {icon: orangeIcon})
+      .addTo(map)
+      .bindPopup("I am an orange leaf.");
   }
 }
